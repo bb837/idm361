597d3096c4aa5e2267b410be64249b88d0f431cd
bb837 <bb837@drexel.edu>
2019-11-07 17:29:52 -0500

    hwk04 Corrections

--- final/nav.js
+++ final/nav.js
@@ -3,13 +3,14 @@
 function openNav(){
     // document.getElementById("cancel").style.visibility = "visable";
     document.getElementById("overlay").style.height = "100%";
-    document.getElementById("nav-icon").style.visibility = "hidden";
+    document.getElementById("nav-icon").style.opacity = "0";
   }
   
   document.getElementById("nav-icon").addEventListener("click", openNav, false);
   
   function closeNav(){
-    document.getElementById("nav-icon").style.visibility = "visable";
     document.getElementById("overlay").style.height = "0%";
+    document.getElementById("nav-icon").style.opacity = "1";
+    console.log('nav-icon');
   }
   document.getElementById("cancel").addEventListener("click",closeNav, false);
